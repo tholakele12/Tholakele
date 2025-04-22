735b36efdb6d2ad76aba133b421f6454f4d65847
tholakele12 <tholakeledube36@gmail.com>
2025-04-22 19:23:42 +0100

    injected more weather data

--- src/index.js
+++ src/index.js
@@ -2,14 +2,50 @@ function refreshWeatherInfo(response) {
     let temperatureElement = document.querySelector("#temperature")
     temperatureElement.innerHTML = response.data.temperature.current
     let temperature=response.data.temperature.current
+    let temperature=response.data.temperature.current;
     let cityElement = document.querySelector("#city")
+    let descriptionElement = document.querySelector("#description")
+    let humidityElement = document.querySelector("#humidity")
+    let windSpeedElement= document.querySelector("#wind-speed")
+    let timeElement= document.querySelector("#time")
+    let date= new Date(response.data.time * 1000)
+    
+
+console.log(response.data.condition.description)
 
 
    cityElement.innerHTML= response.data.city
-    temperatureElement.innerHTML= Math.round(temperature)
+   timeElement.innerHTML= formatDate(date)
+   descriptionElement.innerHTML=response.data.condition.description
+   humidityElement.innerHTML=`${response.data.temperature.humidity}%`
+   windSpeedElement.innerHTML=`${response.data.wind.speed}km/h`
+   temperatureElement.innerHTML= Math.round(temperature)
+
     
 
 }
+function formatDate(date) {
+    
+    let minutes=date.getMinutes()
+    let hours=date.getHours()
+    let  days=  [
+        "Monday",
+        "Tuesday",
+
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday",
+    ]
+    let day = days[date.getDay()]
+    if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+    return `${day} ${hours}:${minutes}`
+}
+
 
 
 function searchCity(city) {
